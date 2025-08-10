34e30649f24fbb63fdcebc8587dc071501ceacec
maxiaolu1981 <15940995655@163.com>
2025-08-10 20:38:00 +0800

    增加doc目录

--- cdmp/backend/pkg/db/mysql.go
+++ cdmp/backend/pkg/db/mysql.go
@@ -1,3 +1,22 @@
+// Copyright (c) 2025 马晓璐
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 // Package db 专注于实现 MySQL 数据库连接的底层创建逻辑，
 // 基于传入的配置参数，封装 GORM 框架的初始化过程，
 // 为上层业务提供可直接用于数据库操作的 *gorm.DB 实例，
